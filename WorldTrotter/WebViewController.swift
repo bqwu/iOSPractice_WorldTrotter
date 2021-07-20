a4bcfa078aee3648fc93bfea563703cf887d15cb
bqwu <bqwuptx@icloud.com>
2021-07-20 20:57:35 +0800

    small update on Bronze Challenge in Chapter 6

--- WorldTrotter/WebViewController.swift
+++ WorldTrotter/WebViewController.swift
@@ -14,17 +14,21 @@ class WebViewController: UIViewController, WKUIDelegate {
     override func loadView() {
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
-
+        
+        webView.scrollView.bounces = false
+        
         view = webView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("WebViewController loaded its view.")
+//        print("WebViewController loaded its view.")
         
+        // three fingers can be used to swipe up/down in the Simulator
         let myURL = URL(string: "https://bignerdranch.com")
 //        let myURL = URL(string: "https://cn.bing.com")
+        
         let myRequest = URLRequest(url: myURL!)
         
         webView.load(myRequest)
