a753036bb3ee62570ea6f606796b7503a211d3ce
bqwu <bqwuptx@icloud.com>
2021-07-22 11:16:15 +0800

    Add the function of locating user's position for Silver Challenge in Chapter 6

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -5,18 +5,37 @@
 //  Created by Baoqiang Wu on 7/15/21.
 //
 
+/*
+You have to set what the location of the simulator actually is:
+   (1) search for “Edit Scheme” in the help menu
+   (2) Options -> Core Location -> Default Location
+*/
+
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
+        
     var mapView: MKMapView!
     
     override func loadView() {
         // create a map view
         mapView = MKMapView()
+        mapView.delegate = self
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
         
+        // comment the line below if automatical locating is desired
+//        mapView.showsUserLocation = true
+
         view = mapView
         
+        segmentedControlCreation()
+        
+        userLocationButtonCreation()
+    }
+    
+    func segmentedControlCreation() {
         let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
         segmentedControl.selectedSegmentIndex = 0
@@ -45,12 +64,6 @@ class MapViewController: UIViewController {
         trailingConstraint.isActive = true
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-//        print("MapViewController loaded its view.")
-    }
-
     @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
         switch segControl.selectedSegmentIndex {
         case 0:
@@ -63,4 +76,53 @@ class MapViewController: UIViewController {
             break
         }
     }
+    
+    func userLocationButtonCreation() {
+        let button: UIButton = UIButton.init(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.frame = CGRect(x: 30, y: 40, width: 30, height: 30)
+
+        button.setTitle("FindMe", for: .normal)
+
+        button.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+
+        button.addTarget(self, action: #selector(showUserLocation(sender:)), for: .touchUpInside)
+
+        view.addSubview(button)
+        
+        let margins = view.layoutMarginsGuide
+
+        let bottomConstraint = button.bottomAnchor.constraint(equalTo: margins.bottomAnchor)
+
+        let leadingConstraint = button.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+
+        bottomConstraint.isActive = true
+        leadingConstraint.isActive = true
+        
+    }
+    
+    @objc func showUserLocation(sender: UIButton!) {
+        print("Start locating the user...")
+        
+        // This should be set here to avoid automatically locating
+        mapView.showsUserLocation = true
+        
+        self.mapView(mapView, didUpdate: mapView.userLocation)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        print("MapViewController loaded its view.")
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+
+        mapView.setRegion(region, animated: true)
+    }
+
 }
