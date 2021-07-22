16c4e7eedd4cb463d8019dd1d5ca1e0449c65337
bqwu <bqwuptx@icloud.com>
2021-07-22 15:09:37 +0800

    add pins and button to switch location for Gold Challenge in Chapter 6

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -26,13 +26,15 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         mapView.isScrollEnabled = true
         
         // comment the line below if automatical locating is desired
-//        mapView.showsUserLocation = true
+        mapView.showsUserLocation = true
 
         view = mapView
         
         segmentedControlCreation()
         
         userLocationButtonCreation()
+        
+        switchLocationButtonCreation()
     }
     
     func segmentedControlCreation() {
@@ -105,24 +107,89 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         print("Start locating the user...")
         
         // This should be set here to avoid automatically locating
-        mapView.showsUserLocation = true
+        // mapView.showsUserLocation = true
         
         self.mapView(mapView, didUpdate: mapView.userLocation)
     }
     
+    let lat1 = 29.6553895, lon1 =  91.1704736
+    let lat2 = 40.0149856, lon2 = -105.270545
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 //        print("MapViewController loaded its view.")
+        
+        addMapPin(lat1, lon1, "Lhasa", "dream place")
+        addMapPin(lat2, lon2, "Boulder", "working place")
     }
     
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         let latitude = userLocation.coordinate.latitude
         let longitude = userLocation.coordinate.longitude
-        let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+
+        showRegion(latitude, longitude)
+        addMapPin(latitude, longitude, "San Francisco", "current location")
+    }
+    
+    func showRegion(_ lat: Double, _ lon: Double) {
+        let center = CLLocationCoordinate2D(latitude: lat, longitude: lon)
         let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
         let region = MKCoordinateRegion(center: center, span: span)
 
         mapView.setRegion(region, animated: true)
     }
+    
+    func addMapPin(_ lat: Double, _ lon: Double, _ title: String, _ subtitle: String) {
+        let center = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = center
+        annotation.title = title
+        annotation.subtitle = subtitle
+        mapView.addAnnotation(annotation)
+    }
 
+    func switchLocationButtonCreation() {
+        let button: UIButton = UIButton.init(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        button.setTitle("Switch", for: .normal)
+
+        button.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+
+        button.addTarget(self, action: #selector(switchLocation(sender:)), for: .touchUpInside)
+
+        view.addSubview(button)
+        
+        let margins = view.layoutMarginsGuide
+
+        let bottomConstraint = button.bottomAnchor.constraint(equalTo: margins.bottomAnchor)
+
+        let trailingConstraint = button.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+
+        bottomConstraint.isActive = true
+        trailingConstraint.isActive = true
+    }
+    
+    var currentLocationIndex = 0
+    let totalLocationNum = 3
+    @objc func switchLocation(sender: UIButton!) {
+        print("Now switch the location...")
+        
+        currentLocationIndex += 1
+        if currentLocationIndex > totalLocationNum {
+            currentLocationIndex -= totalLocationNum
+        }
+        
+        switch currentLocationIndex {
+        case 1:
+            showRegion(lat1, lon1)
+        case 2:
+            showRegion(lat2, lon2)
+        case 3:
+            self.mapView(mapView, didUpdate: mapView.userLocation)
+        default:
+            break
+        }
+    }
+    
 }
