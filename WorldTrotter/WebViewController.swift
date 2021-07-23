87b94f8fbb3b7cdcf8bba08c13480e208f577b55
bqwu <bqwuptx@icloud.com>
2021-07-23 16:53:11 +0800

    improve navigation bar & reorganize codes

--- WorldTrotter/WebViewController.swift
+++ WorldTrotter/WebViewController.swift
@@ -5,22 +5,47 @@
 //  Created by Baoqiang Wu on 7/20/21.
 //
 
+// three fingers can be used to swipe up/down in the Simulator
+
 import UIKit
 import WebKit
 
 class WebViewController: UIViewController, WKNavigationDelegate {
+    
+    let myURL = URL(string: "https://bignerdranch.com")
+//    let myURL = URL(string: "https://cn.bing.com")
+//    let myURL = URL(string: "https://www.apple.com")
+//    let myURL = URL(string: "https://www.jianshu.com/u/040395b7230c")
+    
     var webView = WKWebView()
     
     let navigationBarHeight = 44
+    var navigationBar = UINavigationBar()
+    var barItem = UINavigationItem()
+    
     var statusBarHeight: CGFloat = 0
     
     var progressBar = UIProgressView()
     let progressBarKeyPath = "estimatedProgress"
+    let progressBarHeight = 3
     
     override func viewDidLoad() {
         super.viewDidLoad()
 //        print("WebViewController loaded its view.")
         
+        addWebView()
+        addNavigationBar()
+        addProgressBar()
+        
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
+        view.addSubview(webView)
+        
+        webView.addObserver(self, forKeyPath: progressBarKeyPath, options: .new, context: nil)
+    }
+    
+    func addWebView() {
 //        let statusBarHeight = UIApplication.shared.statusBarFrame.height
 //        let statusBarHeight = view.window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
         let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
@@ -30,48 +55,37 @@ class WebViewController: UIViewController, WKNavigationDelegate {
         
         webView.scrollView.bounces = false
         webView.navigationDelegate = self
-        
-        // three fingers can be used to swipe up/down in the Simulator
-        let myURL = URL(string: "https://bignerdranch.com")
-//        let myURL = URL(string: "https://cn.bing.com")
-//        let myURL = URL(string: "https://www.apple.com")
-//        let myURL = URL(string: "https://www.jianshu.com/u/040395b7230c")
-        
-        let myRequest = URLRequest(url: myURL!)
-        
-        webView.load(myRequest)
-        
-        view.addSubview(webView)
-        
-        progressBar = UIProgressView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 30))
-        progressBar.progress = 0.0
-        progressBar.tintColor = UIColor.blue
-        webView.addSubview(progressBar)
-        
-        webView.addObserver(self, forKeyPath: progressBarKeyPath, options: .new, context: nil)
-    }
+}
         
     func addNavigationBar() {
-        let bar = UINavigationBar(frame: CGRect(x: 0, y: statusBarHeight, width: self.view.frame.width, height: CGFloat(navigationBarHeight)))
+        navigationBar = UINavigationBar(frame: CGRect(x: 0, y: statusBarHeight, width: self.view.frame.width, height: CGFloat(navigationBarHeight)))
         
-        bar.backgroundColor = UIColor.white.withAlphaComponent(0.5)
-        
-        let barItem = UINavigationItem(title: webView.title ?? "No title")
-        
+        navigationBar.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+                
         let backButton = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(toBack))
         let forwardButton = UIBarButtonItem(title: "Forward", style: .plain, target: self, action: #selector(toForward))
         
         barItem.rightBarButtonItem = forwardButton
         barItem.leftBarButtonItem = backButton
         
-        bar.setItems([barItem], animated: false)
+        navigationBar.setItems([barItem], animated: false)
         
-        view.addSubview(bar)
+        view.addSubview(navigationBar)
+    }
+    
+    func addProgressBar() {
+        progressBar = UIProgressView(frame: CGRect(x: 0, y: Int(statusBarHeight) + navigationBarHeight - progressBarHeight, width: Int(self.view.frame.width), height: progressBarHeight))
+        
+        progressBar.progress = 0.0
+        progressBar.tintColor = UIColor.blue
+//        progressBar.alpha = 0.7
+        
+        view.addSubview(progressBar)
     }
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         print(webView.title ?? "No title?")
-        addNavigationBar()
+        barItem.title = webView.title
     }
     
     @objc func toBack() {
@@ -90,21 +104,14 @@ class WebViewController: UIViewController, WKNavigationDelegate {
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if keyPath == self.progressBarKeyPath {
-            self.progressBar.alpha = 1.0
-            self.progressBar.setProgress(Float(webView.estimatedProgress), animated: true)
             if self.webView.estimatedProgress >= 1.0 {
                 UIView.animate(withDuration: 0.3, delay: 0.1, options: .curveEaseInOut, animations: { () -> Void in self.progressBar.alpha = 0.0 }, completion: { (finished: Bool) -> Void in self.progressBar.progress = 0 })
             }
+            else {
+                self.progressBar.alpha = 1.0   // must be here but I don't know why
+                self.progressBar.setProgress(Float(webView.estimatedProgress), animated: true)
+            }
         }
     }
-   
-// not sure how to remove observer, but even without it everything goes well
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        webView.removeObserver(self, forKeyPath: self.progressBarKeyPath)
-    }
-    
-//    deinit {
-//        webView.removeObserver(self, forKeyPath: self.progressBarKeyPath)
-//    }
+
 }
